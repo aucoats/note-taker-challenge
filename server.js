e39a50abd25247b6f35d8d758973bbc2e6c60de1
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-30 12:57:36 -0500

    html routes

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const htmlRoutes = require('./routes/htmlRoutes')
+const express = require('express')
+// const apiRoutes = require('./routes/apiRoutes/noteRoutes')
+const htmlRoutes = require('./routes/htmlRoutes/index')
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static('public'))
+
+// app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`API server now open on port ${PORT}!`)
+})
+
+
+
